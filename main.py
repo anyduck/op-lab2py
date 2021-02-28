481a47b0ac7187c80cf5530c89850418fc13e3ff
Dmytro Herashchenko <51529317+moodduckk@users.noreply.github.com>
2021-02-28 21:03:48 +0200

    Add: convert csv file to list

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 from pathlib import Path
+from collections import namedtuple
+
+
+InputLine = namedtuple('InputLine', ['surname', 'r1', 'r2', 'r3', 'r4', 'r5', 'is_contractor'])
+OutputLine = namedtuple('OutputLine', ['surname', 'rating'])
 
 
 def find_csv(folder: Path) -> list[Path]:
@@ -8,8 +13,17 @@ def find_csv(folder: Path) -> list[Path]:
     return [path for path in folder.iterdir()
             if path.is_file() and path.suffix == '.csv']
 
+def csv2list(path: Path) -> list[InputLine]:
+    result = []
+    with open(path, 'r') as csv:
+        for line in csv.readlines()[1:]: # Пропускаємо перший рядок
+            result.append(InputLine(*line.split(',')))
+    return result
+
+
 def main(folder: Path, output: Path) -> None:
-    print(find_csv(folder))
+    files = find_csv(folder)
+    print(csv2list(files[0]))
 
 
 if __name__ == '__main__':

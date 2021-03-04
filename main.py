31c0b3688f6e5922188bc938f2bf59abab27008b
Dmytro Herashchenko <51529317+dmytro-duck@users.noreply.github.com>
2021-03-04 13:32:17 +0200

    Refactor: reorder function, concatenate calc_rating and calc_stipend

--- main.py
+++ main.py
@@ -10,6 +10,41 @@ InputLine = namedtuple('InputLine', ['surname', 'rating', 'is_contractor'])
 OutputLine = namedtuple('OutputLine', ['surname', 'rating_avg'])
 
 
+def main():
+    args = parse_args()
+    table = load_table(args.folder)
+
+    stipend_table = calc_stipend(table)
+    print(f'Мінімальний рейтинг для стипендії становить: {stipend_table[-1].rating_avg}')
+
+    save_table(stipend_table, args.output)
+
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser(description='Process rating csv files')
+    parser.add_argument('folder', type=Path,
+                        help='folder with csv files')
+    parser.add_argument('-o', '--output', type=Path,
+                        default=Path('rating.csv'),
+                        help='path to the final csv file')
+
+    return parser.parse_args()
+
+
+def load_table(folder: Path) -> list[InputLine]:
+    files = find_csv(folder)
+    table = multiple_csv2table(files)
+
+    return [list2input_line(row) for row in table]
+
+
+def find_csv(folder: Path) -> list[Path]:
+    """ Повертає список всіх *.csv файлів в теці. """
+
+    return [path for path in folder.iterdir()
+            if path.is_file() and path.suffix == '.csv']
+
+
 def list2input_line(line: list) -> InputLine:
     """ Конвертує список вхідних даних в InputLine. """
 
@@ -29,38 +64,14 @@ def list2input_line(line: list) -> InputLine:
     return InputLine(surname, rating, is_contractor)
 
 
-def output_line2list(line: OutputLine) -> list[str]:
-    """ Конвертує кінцевий OuptupLine в список. """
-
-    return [str(x) for x in line]
-
-
-def find_csv(folder: Path) -> list[Path]:
-    """ Повертає список всіх *.csv файлів в теці. """
-
-    return [path for path in folder.iterdir()
-            if path.is_file() and path.suffix == '.csv']
-
-
-def load_table(folder: Path) -> list[InputLine]:
-    files = find_csv(folder)
-    table = multiple_csv2table(files)
-
-    return [list2input_line(row) for row in table]
-
-
-def calc_scholarship_average_rating(table: list[InputLine]) -> list[OutputLine]:
-    """ Обчислює середній бал студентів б'юджетників. """
+def calc_stipend(table: list[InputLine]) -> list[OutputLine]:
+    """ Обчислює список студентів, що отримають спипендію. """
 
     def average(rating):
         return round(sum(rating) / len(rating), 3)
 
-    return [OutputLine(student.surname, average(student.rating))
-            for student in table if not student.is_contractor]
-
-
-def calc_stipend(table: list[OutputLine]) -> list[OutputLine]:
-    """ Обчислює список студентів, що отримають спипендію. """
+    table = [OutputLine(student.surname, average(student.rating))
+             for student in table if not student.is_contractor]
 
     table = sorted(table, key=lambda x: x.rating_avg, reverse=True)
     limit = int(len(table) * 0.4)
@@ -73,25 +84,11 @@ def save_table(table: list[OutputLine], path: Path) -> None:
     table2csv(converted, path)
 
 
-def main(folder: Path, output: Path) -> None:
-    table = load_table(folder)
-    scholarship_table = calc_scholarship_average_rating(table)
-    stipend_table = calc_stipend(scholarship_table)
-    print(f'Мінімальний рейтинг для стипендії становить: {stipend_table[-1].rating_avg}')
-    save_table(stipend_table, output)
+def output_line2list(line: OutputLine) -> list[str]:
+    """ Конвертує кінцевий OuptupLine в список. """
 
-
-def parse_args() -> Namespace:
-    parser = ArgumentParser(description='Process rating csv files')
-    parser.add_argument('folder', type=Path,
-                        help='folder with csv files')
-    parser.add_argument('-o', '--output', type=Path,
-                        default=Path('rating.csv'),
-                        help='path to the final csv file')
-
-    return parser.parse_args()
+    return [str(x) for x in line]
 
 
 if __name__ == '__main__':
-    args = parse_args()
-    main(args.folder, args.output)
+    main()
